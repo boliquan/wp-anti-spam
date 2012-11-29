5d44a763d9268d324a45498767c15e35d7ebbadf
boliquan <berttung@gmail.com>
2012-11-28 21:01:32 -0800

    version 1.2.5

---
+++ wp-anti-spam.php
@@ -0,0 +1,189 @@
+<?php
+/*
+Plugin Name: WP Anti Spam
+Plugin URI: http://boliquan.com/wp-anti-spam/
+Description: WP Anti Spam can anti automated spambots, it can also anti artificial spams by"comment","ip","name","email","url". It has many other ways to deal with the spam comments. Besides,it can delete its own options, so it is a green plugin ! 
+Version: 1.2.5
+Author: BoLiQuan
+Author URI: http://boliquan.com/
+Text Domain: WP-Anti-Spam
+Domain Path: /lang
+*/
+
+function load_wp_anti_spam_lang(){
+	$currentLocale = get_locale();
+	if(!empty($currentLocale)){
+		$moFile = dirname(__FILE__) . "/lang/wp-anti-spam-" . $currentLocale . ".mo";
+		if(@file_exists($moFile) && is_readable($moFile)) load_textdomain('WP-Anti-Spam', $moFile);
+	}
+}
+add_filter('init','load_wp_anti_spam_lang');
+
+$wp_anti_spam_words = array();
+if(!defined('WASINFO')){define('WASINFO','(Protected by <a href="http://boliquan.com/wp-anti-spam/" target="_blank">WP Anti Spam</a>)');}
+
+function wp_anti_spam($comment_data){
+	
+	$wp_anti_spam_words = explode("\r\n" , get_option("wp_anti_spam_words"));
+	$comment_fields = array('author','email','url','comment');
+
+	foreach($wp_anti_spam_words as $wp_anti_spam_word){
+		$wp_anti_spam_word = trim($wp_anti_spam_word);
+		foreach($comment_fields as $comment_field){
+			if( $wp_anti_spam_word != '' && isset($_POST[$comment_field]) ){
+				if(eregi($wp_anti_spam_word, $_POST[$comment_field]) !== false){
+ 					wp_die(__('Error: The information you submit contains banned words.','WP-Anti-Spam').WASINFO);
+				}
+			}
+		}
+	}
+
+	$visitor_ip = (@getenv("REMOTE_ADDR")) ? getenv("REMOTE_ADDR") : $_SERVER['REMOTE_ADDR'];
+	if(in_array($visitor_ip,$wp_anti_spam_words)){
+		wp_die(__('Error: Your IP has been banned to leave comments.','WP-Anti-Spam').WASINFO);
+	}
+
+	if(get_option("wp_anti_spam_links")=='yes'){
+		$links = '/http:\/\//u';
+		if(preg_match($links, $comment_data['comment_content'])){
+			wp_die(__('Error: Links are not allowed in comments.','WP-Anti-Spam').WASINFO);
+		}
+	}
+
+	if(get_option("wp_anti_spam_cn_must")=='yes'){
+		$cn_char = '/[一-龥]/u';
+		if(!preg_match($cn_char, $comment_data['comment_content'])){
+			wp_die(__('Error: Comments must contain Chinese.','WP-Anti-Spam').WASINFO);
+		}
+	}
+
+	return($comment_data);
+}
+
+add_filter('preprocess_comment','wp_anti_spam');
+?>
+<?php if(is_admin()){require_once('wp_anti_spam_admin.php');} ?>
+<?php
+if(get_option("wp_anti_spam_hiddenfield")!=''){
+	if(!isset($_SESSION)){
+		session_start();
+		session_regenerate_id();
+	}
+	function wp_anti_spam_hidden(){
+		$unique_value = md5(uniqid(rand(),TRUE));
+		$_SESSION['unique_session'] = $unique_value;
+		echo '<input type="hidden" name="hidden_form" value="'.$unique_value.'" />';
+	}
+	add_action('comment_form','wp_anti_spam_hidden');
+	function wp_anti_spam_hiddenfield($hidden_check){
+		$check_result = 0;
+		if(isset($_SESSION['unique_session'])){
+			if(!isset($_POST['hidden_form']) || $_SESSION['unique_session'] != $_POST['hidden_form']){
+				$check_result = 1;
+			}
+		}
+		elseif(!isset($_SESSION['unique_session'])){
+			$check_result = 1;
+		}
+		if($check_result){
+			wp_die(__('Error: You are a automated spambot or your browser does not enable Cookie.','WP-Anti-Spam').WASINFO);
+		}
+		return $hidden_check;
+	}
+	add_filter('preprocess_comment','wp_anti_spam_hiddenfield');
+}
+
+if(get_option("wp_anti_spam_gravatar")!='no-treatment'){
+	function was_app_get_html($url){
+		$curl = curl_init($url);
+		$useragent="Mozilla/5.0 (Windows NT 5.1; rv:6.0.1) Gecko/20100101 Firefox/6.0.1";
+		curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
+		curl_setopt ($curl, CURLOPT_USERAGENT, $useragent);
+		$data = curl_exec($curl);
+		curl_close($curl);
+		return $data;
+	}
+	function was_no_avatar_to_spam($comment){
+		$emaildata = md5( strtolower($comment['comment_author_email']) );
+		$urldata = 'http://www.gravatar.com/avatar/'. $emaildata .'?d=404';
+		$avatarinfo = was_app_get_html( $urldata );
+		if(substr($avatarinfo,0,3)=='404'){
+			if(get_option("wp_anti_spam_gravatar")=='mark-it-as-spam' && !is_user_logged_in()){
+				add_filter('pre_comment_approved', create_function('', 'return "spam";'));
+			}
+			if(get_option("wp_anti_spam_gravatar")=='block-it' && !is_user_logged_in()){
+				wp_die(__('Error: You have to apply for an avatar at Gravatar.com.','WP-Anti-Spam').WASINFO);
+			}
+		}
+		return $comment;
+	}
+	add_action('preprocess_comment', 'was_no_avatar_to_spam', 1);
+}
+
+if(get_option("wp_anti_spam_min")!='' && get_option("wp_anti_spam_max")!=''){
+	function was_comment_length($incoming_comment){
+		if(mb_strlen($incoming_comment['comment_content'],'utf-8')<get_option("wp_anti_spam_min") || mb_strlen($incoming_comment['comment_content'],'utf-8')>get_option("wp_anti_spam_max")){
+			wp_die(__('Error: The words you input does not meet the Word Limit.','WP-Anti-Spam').WASINFO);
+		}
+		return($incoming_comment);
+	}
+	add_filter('preprocess_comment','was_comment_length');
+}
+
+if(get_option("wp_anti_spam_nicknames")!='' || get_option("wp_anti_spam_emails")!=''){
+	function was_commenter_check($incoming_comment){
+		$isposing = 0;
+		$wp_anti_spam_nicknames = explode("," , get_option("wp_anti_spam_nicknames"));
+		foreach($wp_anti_spam_nicknames as $wp_anti_spam_nickname){
+			if($incoming_comment['comment_author'] == trim($wp_anti_spam_nickname)) { $isposing = 1; }
+		}
+		$wp_anti_spam_emails = explode("," , get_option("wp_anti_spam_emails"));
+		foreach($wp_anti_spam_emails as $wp_anti_spam_email){
+			if($incoming_comment['comment_author_email'] == trim($wp_anti_spam_email)) { $isposing = 1; }
+		}
+		if(!$isposing || is_user_logged_in()) { return $incoming_comment; }
+		wp_die(__('Error: Only the logged user can use this nickname or email.','WP-Anti-Spam').WASINFO);
+	}
+	add_filter('preprocess_comment','was_commenter_check');
+}
+
+function wp_anti_spam_activate(){
+	add_option('wp_anti_spam_words','');
+	add_option('wp_anti_spam_links','yes');
+	add_option('wp_anti_spam_cn_must','');
+	add_option('wp_anti_spam_hiddenfield','');
+	add_option('wp_anti_spam_gravatar','mark-it-as-spam');
+	add_option('wp_anti_spam_min','0');
+	add_option('wp_anti_spam_max','9999');
+	add_option('wp_anti_spam_nicknames','');
+	add_option('wp_anti_spam_emails','');
+	add_option('wp_anti_spam_deactivate','');
+	add_option('wp_anti_spam_support','yes');
+}
+register_activation_hook( __FILE__, 'wp_anti_spam_activate' );
+
+if(get_option("wp_anti_spam_deactivate")=='yes'){
+	function wp_anti_spam_deactivate(){
+		global $wpdb;
+		$remove_options_sql = "DELETE FROM $wpdb->options WHERE $wpdb->options.option_name like 'wp_anti_spam_%'";
+		$wpdb->query($remove_options_sql);
+	}
+	register_deactivation_hook(__FILE__,'wp_anti_spam_deactivate');
+}
+
+if(get_option("wp_anti_spam_support")!='no'){
+	function support_author(){
+		echo '<div style="text-align:center; font-style:italic; font-size:12px;">Protected by <a href="http://boliquan.com/wp-anti-spam/" target="_blank">WP Anti Spam</a></div>';
+	}
+	add_action('comment_form','support_author');
+}
+
+function wp_anti_spam_settings_link($action_links,$plugin_file){
+	if($plugin_file==plugin_basename(__FILE__)){
+		$was_settings_link = '<a href="options-general.php?page=' . dirname(plugin_basename(__FILE__)) . '/wp_anti_spam_admin.php">' . __("Settings") . '</a>';
+		array_unshift($action_links,$was_settings_link);
+	}
+	return $action_links;
+}
+add_filter('plugin_action_links','wp_anti_spam_settings_link',10,4); 
+?>
---
+++ wp_anti_spam_admin.php
@@ -0,0 +1,194 @@
+<?php
+function wp_anti_spam_admin(){
+	add_options_page('WP Anti Spam Options', 'WP Anti Spam','manage_options', __FILE__, 'wp_anti_spam_options');
+	add_action('admin_init','wp_anti_spam_register' );
+}
+function wp_anti_spam_register(){
+	register_setting('was-settings','wp_anti_spam_words');
+	register_setting('was-settings','wp_anti_spam_links');
+	register_setting('was-settings','wp_anti_spam_cn_must');
+	register_setting('was-settings','wp_anti_spam_hiddenfield');
+	register_setting('was-settings','wp_anti_spam_gravatar');
+	register_setting('was-settings','wp_anti_spam_min');
+	register_setting('was-settings','wp_anti_spam_max');
+	register_setting('was-settings','wp_anti_spam_nicknames');
+	register_setting('was-settings','wp_anti_spam_emails');
+	register_setting('was-settings','wp_anti_spam_deactivate');
+	register_setting('was-settings','wp_anti_spam_support');
+}
+
+function wp_anti_spam_options(){
+?>
+<div class="wrap">
+	
+<?php screen_icon(); ?>
+<h2>WP Anti Spam</h2>
+<div style="text-align:center;"><a href="http://wordpress.org/extend/plugins/wp-anti-spam/" target="_blank">How to use WP Anti Spam ?</a> | <a href="http://boliquan.com/wp-anti-spam/" target="_blank">Homepage</a></div>
+
+<form action="options.php" method="post" enctype="multipart/form-data" name="wp_anti_spam_form">
+<?php settings_fields('was-settings'); ?>
+
+<table class="form-table">
+	<tr valign="top">
+		<th scope="row">
+			<?php _e('Banned Words (one per line)','WP-Anti-Spam'); ?>
+		</th>
+		<td>
+			<label>
+				<textarea type="text" name="wp_anti_spam_words" cols="50" rows="12" style="width:86%;font-size:12px;"><?php echo get_option('wp_anti_spam_words'); ?></textarea>
+				<br /><?php _e('Comments will be banned contain above words, no matter the words appear in "comment","ip","name","email","url".','WP-Anti-Spam'); ?>
+			</label>
+		</td>
+	</tr>
+	<tr valign="top">
+		<th scope="row">
+			<?php _e('Disallow Links','WP-Anti-Spam'); ?>
+		</th>
+		<td>
+			<label>
+				<input type="checkbox" name="wp_anti_spam_links" value="yes" <?php if(get_option("wp_anti_spam_links")=='yes') echo 'checked="checked"'; ?> />
+				<?php _e("Disallow the comments that contain links.",'WP-Anti-Spam'); ?>
+			</label>
+		</td>
+	</tr>
+	<tr valign="top">
+		<th scope="row">
+			<?php _e('Some Chinese','WP-Anti-Spam'); ?>
+		</th>
+		<td>
+			<label>
+				<input type="checkbox" name="wp_anti_spam_cn_must" value="yes" <?php if(get_option("wp_anti_spam_cn_must")=='yes') echo 'checked="checked"'; ?> />
+				<code><?php _e("Notice: If you do not speak chinese, please don't check this.",'WP-Anti-Spam'); ?></code>
+			</label>
+		</td>
+	</tr>
+	<tr valign="top">
+		<th scope="row">
+			<?php _e('Generate Hidden Field','WP-Anti-Spam'); ?>
+		</th>
+		<td>
+			<label>
+				<input type="checkbox" name="wp_anti_spam_hiddenfield" value="yes" <?php if(get_option("wp_anti_spam_hiddenfield")=='yes') echo 'checked="checked"'; ?> />
+				<?php _e("Generate an hidden field to anti spambots,but if you open the cache or your browser's Cookie are not enabled,please don't check this.",'WP-Anti-Spam'); ?>
+			</label>
+		</td>
+	</tr>
+	<tr valign="top">
+		<th scope="row">
+			<?php _e('When No Gravatar','WP-Anti-Spam'); ?>
+		</th>
+		<td>
+			<label>
+				<input name="wp_anti_spam_gravatar" type="radio" value="no-treatment"<?php if (get_option('wp_anti_spam_gravatar') == 'no-treatment') { ?> checked="checked"<?php } ?> />
+				<?php _e('no treatment','WP-Anti-Spam'); ?>
+			</label>
+			&nbsp;
+			<label>
+				<input name="wp_anti_spam_gravatar" type="radio" value="mark-it-as-spam"<?php if (get_option('wp_anti_spam_gravatar') == 'mark-it-as-spam') { ?> checked="checked"<?php } ?> />
+				<?php _e('mark it as spam','WP-Anti-Spam'); ?>
+			</label>
+			&nbsp;
+			<label>
+				<input name="wp_anti_spam_gravatar" type="radio" value="block-it"<?php if (get_option('wp_anti_spam_gravatar') == 'block-it') { ?> checked="checked"<?php } ?> />
+				<?php _e('block it','WP-Anti-Spam'); ?>
+			</label>
+		</td>
+	</tr>
+	<tr valign="top">
+		<th scope="row">
+			<?php _e('Word Limit','WP-Anti-Spam'); ?>
+		</th>
+		<td>
+			<label>
+				<?php _e('min','WP-Anti-Spam'); ?>
+				<input type="text" name="wp_anti_spam_min" value="<?php echo get_option('wp_anti_spam_min'); ?>" size="8" />
+			</label>
+			&nbsp;
+			<label>
+				<?php _e('max','WP-Anti-Spam'); ?>
+				<input type="text" name="wp_anti_spam_max" value="<?php echo get_option('wp_anti_spam_max'); ?>" size="8" />
+			</label>
+		</td>
+	</tr>
+	<tr valign="top">
+		<th scope="row">
+			<?php _e('Stop Posing (separated by commas)','WP-Anti-Spam'); ?>
+		</th>
+		<td>
+			<label>
+				<?php _e('protected nicknames','WP-Anti-Spam'); ?>
+				<input type="text" name="wp_anti_spam_nicknames" value="<?php echo get_option('wp_anti_spam_nicknames'); ?>" size="26" />
+			</label>
+			&nbsp;
+			<label>
+				<?php _e('protected emails','WP-Anti-Spam'); ?>
+				<input type="text" name="wp_anti_spam_emails" value="<?php echo get_option('wp_anti_spam_emails'); ?>" size="26" />
+			</label>
+		</td>
+	</tr>
+	<tr valign="top">
+		<th scope="row">
+			<?php _e('Delete Options','WP-Anti-Spam'); ?>
+		</th>
+		<td>
+			<label>
+				<input type="checkbox" name="wp_anti_spam_deactivate" value="yes" <?php if(get_option("wp_anti_spam_deactivate")=='yes') echo 'checked="checked"'; ?> />
+				<?php _e('Delete options while deactivate this plugin.','WP-Anti-Spam'); ?>
+			</label>
+		</td>
+	</tr>
+	<tr valign="top">
+		<th scope="row">
+			Support Author
+		</th>
+		<td>
+			<select name="wp_anti_spam_support">
+				<option value="yes" <?php if (get_option('wp_anti_spam_support') == 'yes') { echo 'selected="selected"'; } ?>>support author</option>
+				<option value="no" <?php if (get_option('wp_anti_spam_support') == 'no') { echo 'selected="selected"'; } ?>>do not support</option>
+			</select>
+		</td>
+	</tr>
+</table>
+
+<p class="submit">
+<input type="submit" class="button-primary" name="Submit" value="<?php _e('Save Changes'); ?>" />
+</p>
+
+</form>
+
+<br />
+<?php $donate_url = plugins_url('/img/paypal_32_32.jpg', __FILE__);?>
+<?php $paypal_donate_url = plugins_url('/img/btn_donateCC_LG.gif', __FILE__);?>
+<?php $ali_donate_url = plugins_url('/img/alipay_donate.png', __FILE__);?>
+<div class="icon32"><img src="<?php echo $donate_url; ?>" alt="Donate" /></div>
+<h2>Donate</h2>
+<p>
+If you find my work useful and you want to encourage the development of more free resources, you can do it by donating.
+</p>
+<p>
+<a href="https://www.paypal.com/cgi-bin/webscr?cmd=_donations&business=SKA6TPPWSATKG&item_name=BoLiQuan&no_shipping=0&no_note=1&tax=0&currency_code=USD&lc=CA&bn=PP%2dDonationsBF&charset=UTF%2d8" target="_blank"><img src="<?php echo $paypal_donate_url; ?>" alt="Paypal Donate" title="Paypal" /></a>
+&nbsp;
+<a href="https://me.alipay.com/boliquan" target="_blank"><img src="<?php echo $ali_donate_url; ?>" alt="Alipay Donate" title="Alipay" /></a>
+</p>
+<br />
+
+<?php $blq_logo_url = plugins_url('/img/blq_32_32.jpg', __FILE__);?>
+<div class="icon32"><img src="<?php echo $blq_logo_url; ?>" alt="BoLiQuan" /></div>
+<h2>Related Links</h2>
+<ul style="margin:0 18px;">
+<li><a href="http://boliquan.com/wp-anti-spam/" target="_blank">WP-Anti-Spam (FAQ)</a> | <a href="http://wordpress.org/extend/plugins/wp-anti-spam/" target="_blank">Download</a></li>
+<li><a href="http://boliquan.com/wp-smtp/" target="_blank">WP SMTP</a> | <a href="http://wordpress.org/extend/plugins/wp-smtp/" target="_blank">Download</a></li>
+<li><a href="http://boliquan.com/wp-code-highlight/" target="_blank">WP Code Highlight</a> | <a href="http://wordpress.org/extend/plugins/wp-code-highlight/" target="_blank">Download</a></li>
+<li><a href="http://boliquan.com/wp-slug-translate/" target="_blank">WP Slug Translate</a> | <a href="http://wordpress.org/extend/plugins/wp-slug-translate/" target="_blank">Download</a></li>
+<li><a href="http://boliquan.com/yg-share/" target="_blank">YG Share</a> | <a href="http://wordpress.org/extend/plugins/yg-share/" target="_blank">Download</a></li>
+<li><a href="http://boliquan.com/ylife/" target="_blank">YLife</a> | <a href="http://code.google.com/p/ylife/downloads/list" target="_blank">Download</a></li>
+<li><a href="http://boliquan.com/" target="_blank">BoLiQuan</a></li>
+</ul>
+
+<div style="text-align:center; margin:60px 0 10px 0;">&copy; <?php echo date("Y"); ?> BoLiQuan</div>
+
+</div>
+<?php 
+}
+add_action('admin_menu', 'wp_anti_spam_admin');
+?>
